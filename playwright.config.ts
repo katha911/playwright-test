ece74947ffdc112ce21cf1c15c27264cd0e12649
kathawut mangkalad <tamza1094@hotmail.com>
2022-12-06 17:28:04 +0700

    report CI 03

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'html',
+  reporter: [['html', { outputFolder: 'my-report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
 
