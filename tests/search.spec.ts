180f94717bdc2f545ba65223ab8217643baeca15
kathawut mangkalad <tamza1094@hotmail.com>
2022-12-06 15:43:33 +0700

    CI report21

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('google search', async ({ page }) => {
-    await page.goto('https://www.google.com/');
-    await page.getByRole('combobox', { name: 'ค้นหา' }).click();
-    await page.getByRole('combobox', { name: 'ค้นหา' }).fill('youtube');
-    await page.getByRole('combobox', { name: 'ค้นหา' }).press('Enter');
-    await page.getByRole('link', { name: 'YouTube https://www.youtube.com › ...' }).click();
-});
+// test('google search', async ({ page }) => {
+//     await page.goto('https://www.google.com/');
+//     await page.getByRole('combobox', { name: 'ค้นหา' }).click();
+//     await page.getByRole('combobox', { name: 'ค้นหา' }).fill('youtube');
+//     await page.getByRole('combobox', { name: 'ค้นหา' }).press('Enter');
+//     await page.getByRole('link', { name: 'YouTube https://www.youtube.com › ...' }).click();
+// });
 test('youtube search', async ({ page }) => {
     await page.goto('https://www.youtube.com/');
     await page.locator('#search-form #container').click();
