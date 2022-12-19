72d4f4e6a3f83f2fde27444490922d200b7c33c7
kathawut mangkalad <tamza1094@hotmail.com>
2022-12-19 16:39:01 +0700

    test

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect} from '@playwright/test';
 test('google', async ({ page }) => {
 
-  await page.goto('https://www.google.com/', { timeout: 3000 });
-  await page.getByRole('combobox', { name: 'ค้นหา' }).click();
-  await page.getByRole('combobox', { name: 'ค้นหา' }).fill('playwright');
-  await page.getByRole('combobox', { name: 'ค้นหา' }).press('Enter');
+  await page.goto('https://www.google.com');
+  await page.getByRole('combobox', { name: 'Search' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).fill('Playwright');
+  await page.getByRole('combobox', { name: 'Search' }).press('Enter');
   await page.getByRole('link', { name: 'Playwright: Fast and reliable end-to-end testing for modern ... https://playwright.dev' }).click();
   await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'How to install Playwright' }).click();
 });
