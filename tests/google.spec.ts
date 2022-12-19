6467157bc0c17497d5ff31d216927c8fa4a3bc79
kathawut mangkalad <tamza1094@hotmail.com>
2022-12-19 16:23:30 +0700

    add timeout

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
-
+import { test, expect} from '@playwright/test';
 test('google', async ({ page }) => {
-  await page.goto('https://www.google.com/');
+
+  await page.goto('https://www.google.com/', { timeout: 3000 });
   await page.getByRole('combobox', { name: 'ค้นหา' }).click();
   await page.getByRole('combobox', { name: 'ค้นหา' }).fill('playwright');
   await page.getByRole('combobox', { name: 'ค้นหา' }).press('Enter');
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+
   test('youtube search', async ({ page }) => {
     await page.goto('https://www.youtube.com/');
     await page.locator('#search-form #container').click();
